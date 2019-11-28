dbd964a566169af7a898647ba4eea04ae99f9e1c
Guilherme Henrique <guilhermehenrique1099@gmail.com>
2019-11-27 21:17:52 -0300

    Alteração: Método de inicialização.

---
+++ src/main/java/com/guimi/estacionamento/config/DevConfig.java
@@ -0,0 +1,22 @@
+package com.guimi.estacionamento.config;
+
+import com.guimi.estacionamento.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("Dev")
+public class DevConfig {
+    @Autowired
+    private DBService dbService;
+    @Bean
+    public boolean instantiateDataBase() throws ParseException {
+        dbService.instantiateTestDatabase();
+
+        return true;
+    }
+}
