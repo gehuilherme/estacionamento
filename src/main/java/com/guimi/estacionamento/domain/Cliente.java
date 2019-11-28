53662533ed06e76ab268df11687dcea413480cb8
Guilherme Henrique <guilhermehenrique1099@gmail.com>
2019-11-27 21:56:43 -0300

    Alteração: Método de inicialização.

--- src/main/java/com/guimi/estacionamento/domain/Cliente.java
+++ src/main/java/com/guimi/estacionamento/domain/Cliente.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Cliente {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @NotEmpty(message = "Nome não pode estar vazio!")
     private String nome;
